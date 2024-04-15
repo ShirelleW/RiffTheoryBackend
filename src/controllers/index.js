3509b8e28139f4a2192b4bb454eac40e9df0f453
ShirelleW <shirellewinecoffm@gmail.com>
2024-04-15 14:01:10 -0400

    Added updated route tonic/type

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,224 +2,239 @@ import prisma from "../../server.js";
 import { chromatic_notes_flat, chromatic_notes } from "../util/index.js";
 
 export const getScalesByName = async (req, res) => {
-    try {
-        const { name } = req.params;
+  try {
+    const { name } = req.params;
 
-        const scales = await prisma.scales.findUnique({
-            where: {
-                name: name
-            }
-        })
+    const scales = await prisma.scales.findUnique({
+      where: {
+        name: name,
+      },
+    });
 
-        if (scales.length === 0) {
-            res.status(200).json({
-                scales,
-                success: false,
-                message: 'No scales to return!'
-            })
-        } else {
-            res.status(200).json({
-                scales,
-                success: true,
-                message: 'All scales returned'
-            })
-        }
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: `ERROR: ${error.message}`
-        });
+    if (scales.length === 0) {
+      res.status(200).json({
+        scales,
+        success: false,
+        message: "No scales to return!",
+      });
+    } else {
+      res.status(200).json({
+        scales,
+        success: true,
+        message: "All scales returned",
+      });
     }
-}
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: `ERROR: ${error.message}`,
+    });
+  }
+};
 export const getScalesByTonic = async (req, res) => {
+  try {
+    const { tonic } = req.params;
 
-    try {
-        const { tonic } = req.params;
+    // index === true indicates that the note param passed in is either sharp or flat
+    const index = tonic.includes("#")
+      ? chromatic_notes.indexOf(tonic)
+      : tonic.includes("b")
+      ? chromatic_notes_flat.indexOf(tonic)
+      : null;
 
-        // index === true indicates that the note param passed in is either sharp or flat
-        const index = tonic.includes("#")
-            ? chromatic_notes.indexOf(tonic)
-            : tonic.includes('b')
-                ? chromatic_notes_flat.indexOf(tonic)
-                : null
-
-        const scales = index ? await prisma.scales.findMany({
-            where: {
-                tonic: {
-                    in: [chromatic_notes[index], chromatic_notes_flat[index]]
-                }
-            }
-        }) :
-            await prisma.scales.findMany({
-                where: {
-                    tonic: tonic
-                }
-            })
-
-        if (scales.length === 0) {
-            res.status(200).json({
-                scales,
-                success: false,
-                message: 'No scales to return!'
-            })
-        } else {
-            res.status(200).json({
-                scales,
-                success: true,
-                message: 'All scales returned'
-            })
-        }
-    } catch (error) {
-        (error)
-        res.status(400).json({
-            success: false,
-            message: `ERROR: ${error.message}`
-        });
-    }
-}
-
-export const getScalesByType = async (req, res) => {
-
-    try {
-        let { type } = req.params;
-
-        type = type.replace(/\s+/g, " & ");
-
-        const scales = await prisma.scales.findMany({
-            where: {
-                name: {
-                    search: type
-                }
-            }
+    const scales = index
+      ? await prisma.scales.findMany({
+          where: {
+            tonic: {
+              in: [chromatic_notes[index], chromatic_notes_flat[index]],
+            },
+          },
         })
-
-        if (scales.length === 0) {
-            res.status(200).json({
-                scales,
-                success: false,
-                message: 'No scales to return!'
-            })
-        } else {
-            res.status(200).json({
-                scales,
-                success: true,
-                message: 'All scales returned'
-            })
-        }
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: `ERROR: ${error.message}`
+      : await prisma.scales.findMany({
+          where: {
+            tonic: tonic,
+          },
         });
+
+    if (scales.length === 0) {
+      res.status(200).json({
+        scales,
+        success: false,
+        message: "No scales to return!",
+      });
+    } else {
+      res.status(200).json({
+        scales,
+        success: true,
+        message: "All scales returned",
+      });
     }
-}
+  } catch (error) {
+    error;
+    res.status(400).json({
+      success: false,
+      message: `ERROR: ${error.message}`,
+    });
+  }
+};
+
+export const getScalesByTypeAndTonic = async (req, res) => {
+  try {
+    let { type, tonic } = req.params;
+
+    // index === true indicates that the note param passed in is either sharp or flat
+    const index = tonic.includes("#")
+      ? chromatic_notes.indexOf(tonic)
+      : tonic.includes("b")
+      ? chromatic_notes_flat.indexOf(tonic)
+      : null;
+
+    type = type.replace(/\s+/g, " & ");
+
+    const scales = index
+      ? await prisma.scales.findMany({
+          where: {
+            name: {
+                search: type,
+              },
+            tonic: {
+              in: [chromatic_notes[index], chromatic_notes_flat[index]],
+            },
+          },
+        })
+      : await prisma.scales.findMany({
+          where: {
+            name: {
+                search: type,
+              },
+            tonic: tonic,
+          },
+        });
+
+    if (scales.length === 0) {
+      res.status(200).json({
+        scales,
+        success: false,
+        message: "No scales to return!",
+      });
+    } else {
+      res.status(200).json({
+        scales,
+        success: true,
+        message: "All scales returned",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: `ERROR: ${error.message}`,
+    });
+  }
+};
 
 export const getScalesByNameAndTonic = async (req, res) => {
+  try {
+    let { name, tonic } = req.params;
 
-    try {
-        let { name, tonic } = req.params;
+    name = name.replace(/\s+/g, " & ");
 
-        name = name.replace(/\s+/g, " & ");
+    // index === true indicates that the note param passed in is either sharp or flat
+    const index = tonic.includes("#")
+      ? chromatic_notes.indexOf(tonic)
+      : tonic.includes("b")
+      ? chromatic_notes_flat.indexOf(tonic)
+      : null;
 
-        // index === true indicates that the note param passed in is either sharp or flat
-        const index = tonic.includes("#")
-            ? chromatic_notes.indexOf(tonic)
-            : tonic.includes('b')
-                ? chromatic_notes_flat.indexOf(tonic)
-                : null
-
-        const scales = index ? await prisma.scales.findMany({
-            where: {
-                AND: {
-                    name: {
-                        search: name
-                    },
-                    tonic: {
-                        in: [chromatic_notes[index], chromatic_notes_flat[index]]
-                    }
-                }
-            }
-        }) :
-            await prisma.scales.findMany({
-                where: {
-                    AND: {
-                        name: {
-                            search: name
-                        },
-                        tonic: tonic
-                    }
-
-                }
-            })
-        if (scales.length === 0) {
-            res.status(200).json({
-                scales,
-                success: false,
-                message: 'No scales to return!'
-            })
-        } else {
-            res.status(200).json({
-                scales,
-                success: true,
-                message: 'All scales returned'
-            })
-        }
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: `ERROR: ${error.message}`
+    const scales = index
+      ? await prisma.scales.findMany({
+          where: {
+            AND: {
+              name: {
+                search: name,
+              },
+              tonic: {
+                in: [chromatic_notes[index], chromatic_notes_flat[index]],
+              },
+            },
+          },
+        })
+      : await prisma.scales.findMany({
+          where: {
+            AND: {
+              name: {
+                search: name,
+              },
+              tonic: tonic,
+            },
+          },
         });
+    if (scales.length === 0) {
+      res.status(200).json({
+        scales,
+        success: false,
+        message: "No scales to return!",
+      });
+    } else {
+      res.status(200).json({
+        scales,
+        success: true,
+        message: "All scales returned",
+      });
     }
-}
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: `ERROR: ${error.message}`,
+    });
+  }
+};
 
 export const getByNotesInScale = async (req, res) => {
-    try {
-        let { notes } = req.params;
+  try {
+    let { notes } = req.params;
 
-        notes = [...notes.split(" ").sort()];
+    notes = [...notes.split(" ").sort()];
 
-        let query = [];
+    let query = [];
 
-        for (let i = 0; i < notes.length; i++) {
-            query.push(
-                {
-                    notesinscale: {
-                        contains: notes[i]
-                    }
-                }
-            )
-        }
-
-        let scales = await prisma.scales.findMany({
-            where: {
-                AND: query
-            },
-            take: 20
-        })
-
-        const filteredScales = scales.filter(obj => {
-            const objNotes = obj.notesinscale.split(",")
-            const filter = notes.every(note => objNotes.indexOf(note) > -1)
-
-            return filter
-        })
-
-        if (filteredScales.length === 0) {
-            res.status(200).json({
-                success: false,
-                message: 'No scales to return!'
-            })
-        } else {
-            res.status(200).json({
-                filteredScales,
-                success: true,
-                message: 'All scales returned'
-            })
-        }
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: `ERROR: ${error.message}`
-        });
+    for (let i = 0; i < notes.length; i++) {
+      query.push({
+        notesinscale: {
+          contains: notes[i],
+        },
+      });
     }
-}
+
+    let scales = await prisma.scales.findMany({
+      where: {
+        AND: query,
+      },
+      take: 20,
+    });
+
+    const filteredScales = scales.filter((obj) => {
+      const objNotes = obj.notesinscale.split(",");
+      const filter = notes.every((note) => objNotes.indexOf(note) > -1);
+
+      return filter;
+    });
+
+    if (filteredScales.length === 0) {
+      res.status(200).json({
+        success: false,
+        message: "No scales to return!",
+      });
+    } else {
+      res.status(200).json({
+        filteredScales,
+        success: true,
+        message: "All scales returned",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: `ERROR: ${error.message}`,
+    });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import { Router } from "express" ;
-import { getScalesByTonic, getScalesByType, getScalesByNameAndTonic,
+import { getScalesByTonic, getScalesByTypeAndTonic, getScalesByNameAndTonic,
             getByNotesInScale, getScalesByName } from '../controllers/index.js'
 // Scales, type: major/minor/etc, root: root note
 // req.params: { "type": major/minor/etc, "root": rootno te }
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get("/tonic/:tonic", getScalesByTonic);
 router.get("/name/:name", getScalesByName)
-router.get("/type/:type", getScalesByType);
+router.get("/typeandtonic/:type/:tonic", getScalesByTypeAndTonic);
 router.get("/tonicandname/:tonic/:name", getScalesByNameAndTonic)
 router.get("/filterbynotes/:notes", getByNotesInScale)
 // router.get("/filterbynotes", getScalesByNotesInScale);
